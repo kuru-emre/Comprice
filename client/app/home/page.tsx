a1356be826dba9a87a5e136f44600f936441ba3e
Front Desk <frontdesk@AQUALITY.local>
2024-02-26 20:05:46 -0400

    Implemented shadcn for ui components
    Signed-off-by: Front Desk <frontdesk@AQUALITY.local>

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <>
-    
+      test
     </>
   );
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header>
+            <nav>
+              <Navbar />
+            </nav>
+          </header>
+          <main>
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
