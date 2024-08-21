3d04a13cb8429f7cd3cc73909203d7ecf05dfbf1
kuru-emre <kuru.emre@hotmail.com>
2024-08-21 19:40:23 -0300

    Remove autoprefixer it causes build errors
    Signed-off-by: kuru-emre <kuru.emre@hotmail.com>

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    autoprefixer: {},
     tailwindcss: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: 'advanced', discardComments: { removeAll: true } } } : {}),
   },
