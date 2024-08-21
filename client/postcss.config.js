b84f5cbd6437ce92904baa74b4bd01cc12049b86
kuru-emre <kuru.emre@hotmail.com>
2024-08-21 19:51:10 -0300

    testing build error

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,9 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
+    autoprefixer: {},
     tailwindcss: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: 'advanced', discardComments: { removeAll: true } } } : {}),
-  },
-};
-
-export default config;
+  }
+}
